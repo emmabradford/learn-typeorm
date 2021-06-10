31400b30b82e056303c4754236a525c5f0ba56bf
EmmaAbe <emma.abe.bradford@gmail.com>
2021-06-10 10:35:36 -0500

    connected code to mysql

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,15 @@ import {createConnection} from "typeorm";
 import {User} from "./entity/User";
 
 createConnection().then(async connection => {
-
-    console.log("Inserting a new user into the database...");
     const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
 
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
+    user.lastName = "Wei";
+    user.firstName = "Ying";
+    user.email = "Wuxian@BurialMounds.com";
+    user.role = "Yiling Laozu";
 
-    console.log("Here you can setup and run express/koa/any other framework.");
+    await user.save()
+
+    console.log("Mo xuanyu is dead. Wei Ying is back, baby! DOon't tell Lan Zhan");
 
 }).catch(error => console.log(error));
